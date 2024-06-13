0e2e66a766bd55f8635d5a0e57a1534dabc435fa
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2024-06-13 15:44:50 -0300

    ajustes

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const copiar = document.querySelector('#btn-copiar');
 const contador = document.querySelector('#contador');
 
 
-input.addEventListener('input', function(){
-	const text = this.value;
-	contador.innerText = text.length + '/300'; 
-}); 
+
 
 copiar.addEventListener('click', function(){
 	input.select();
@@ -19,5 +16,36 @@ copiar.addEventListener('click', function(){
 limpar.addEventListener('click', function(){
 	input.value = ' ';
 	contador.innerText = '0/300';
+	input.disabled = false;
+	contador.classList.remove('text-red-600')
+	contador.classList.add('text-gray-600')
 	
 })
+
+
+function travarteclas () {
+	input.disabled = true
+	input.addEventListener("keydown", (e) => {e.preventDefault})
+	input.addEventListener("paste", (e) => {e.preventDefault})
+}
+
+
+input.addEventListener('input', function(){
+	const text = this.value;
+	contador.innerText = text.length + '/300'; 
+
+	if(text.length >= 300 ) { 
+		alert("Mais de 300 caracteres!");
+		contador.classList.remove('text-gray-600')
+		contador.classList.add('text-red-600')
+		
+		travarteclas()
+
+	} else {
+		contador.classList.remove('text-red-600')
+		contador.classList.add('text-gray-600')
+	}
+
+	
+		
+}); 
